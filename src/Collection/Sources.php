fdaca6c38d06ab7b8bc4cdce0d010f0f03ea9dad
Girvydas Bartkus <girvydas@futureit.lt>
2021-01-20 14:22:14 +0200

    Fix LeadsStatus and Source models with collection for data fetch - set correct URL part for collections.

--- src/Collection/Sources.php
+++ src/Collection/Sources.php
@@ -4,8 +4,6 @@ namespace Teamgate\Collection;
 
 class Sources extends Collection
 {
-    protected $_uri = 'sources';
-
     public function fetchRelation($objectUri, $id)
     {
         $this->_uri = $objectUri . '/' . (int) $id . '/' . 'sources';
--- src/Collection/SourcesList.php
+++ src/Collection/SourcesList.php
@@ -5,4 +5,6 @@ namespace Teamgate\Collection;
 class SourcesList extends Collection
 {
     protected $_model = 'source';
+    protected $_uri = 'sources';
+
 }
